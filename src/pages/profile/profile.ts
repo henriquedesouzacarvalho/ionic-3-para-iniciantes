bf0865da464e2100cbf99bd170b8324250965ffc
Henrique de Souza Carvalho <henrique.carvalho@hipcom.com.br>
2018-01-09 09:24:48 -0200

    Utilizando o plugin da Camera no Ionic

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -1,18 +1,38 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Camera, CameraOptions } from '@ionic-native/camera';
 
 @IonicPage()
 @Component({
   selector: 'page-profile',
   templateUrl: 'profile.html',
+  providers: [
+    Camera
+  ]
 })
 export class ProfilePage {
+  img = "";
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private camera: Camera) {
   }
 
   ionViewDidLoad() {
 
+  }
+
+  takePicture() {
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.PNG,
+      mediaType: this.camera.MediaType.PICTURE
+    }
+
+    this.camera.getPicture(options).then((imageData) => {
+      this.img = 'data:image/jpeg;base64,' + imageData;
+    }, (err) => {
+      // Handle error
+    });
   }
 
 }
